933e2528a92f76ae23f88edff0b11b1024b03d08
tgg <qazplmtfgcv123@gmail.com>
2025-02-14 17:18:03 +0900

    Feat: JwtProperties 클래스 작성

---
+++ src/main/java/com/example/news_feed/auth/JwtProperties.java
@@ -0,0 +1,16 @@
+package com.example.news_feed.auth;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:application-jwt/properties")
+public class JwtProperties {
+
+    @Value("${jwt.seceret_key}")
+    private String secretKey;
+
+    @Value("${jwt.passward_salt}")
+    private String passwordSalt;
+}
