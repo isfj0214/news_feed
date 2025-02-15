33c49176b48178292d2aecbadb7582c0e7dde42c
tgg <qazplmtfgcv123@gmail.com>
2025-02-15 14:33:22 +0900

    CHore: LoginRequestDto 에 유효성 검사 작성

--- src/main/java/com/example/news_feed/auth/dto/LoginRequestDto.java
+++ src/main/java/com/example/news_feed/auth/dto/LoginRequestDto.java
@@ -1,9 +1,12 @@
 package com.example.news_feed.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class LoginRequestDto {
+    @NotBlank(message = "email은 필수 입력 값입니다.")
     private String email;
+    @NotBlank(message = "password는 필숫 입력 갑입니다.")
     private String password;
 }
