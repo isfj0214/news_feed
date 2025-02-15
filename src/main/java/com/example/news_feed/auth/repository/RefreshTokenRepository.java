5da4f0e4556daa5d87b7029f4bab89dec2d2592b
tgg <qazplmtfgcv123@gmail.com>
2025-02-15 13:08:06 +0900

    Feat: RefreshTokenRepository 작성

--- src/main/java/com/example/news_feed/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/news_feed/auth/repository/RefreshTokenRepository.java
@@ -1,7 +1,9 @@
 package com.example.news_feed.auth.repository;
 
+import com.example.news_feed.auth.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RefreshTokenRepository {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 }
