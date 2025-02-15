55bf999e901f0eb43be6cfd57143e5ae9b21bda2
tgg <qazplmtfgcv123@gmail.com>
2025-02-15 14:25:25 +0900

    Feat: ErrorCode 작성

---
+++ src/main/java/com/example/news_feed/common/error/ErrorCode.java
@@ -0,0 +1,42 @@
+package com.example.news_feed.common.error;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    // 400 에러
+
+    // 401 에러
+    INVALID_PASSWORD("비밀번호가 잘못되었습니다.", HttpStatus.UNAUTHORIZED),
+    LOGIN_REQUIRED("로그인이 필요합니다.", HttpStatus.UNAUTHORIZED),
+
+    // 403
+    USER_ACCESS_DENIED("유저에 대한 접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    SCHEDULE_ACCESS_DENIED("일정에 대한 접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    COMMENT_ACCESS_DENIED("댓글에 대한 접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
+
+    // 404 에러
+    COMMENT_NOT_FOUND("commentId에 해당하는 댓글이 없습니다.", HttpStatus.NOT_FOUND),
+    SCHEDULE_NOT_FOUND("scheduleId에 해당하는 일정이 없습니다.", HttpStatus.NOT_FOUND),
+    USER_NOT_FOUND("userId에 해당하는 유저가 없습니다.", HttpStatus.NOT_FOUND),
+    USER_NOT_FOUND_BY_EMAIL("해당 이메일로 등록된 유저가 없습니다.", HttpStatus.NOT_FOUND),
+
+    //409 에러
+    EMAIL_ALREADY_EXISTS("이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT),
+    ALREADY_LOGGED_IN("이미 로그인된 상태입니다.", HttpStatus.CONFLICT);;
+
+    private String message;
+    private HttpStatus httpStatus;
+
+    private ErrorCode(String message, HttpStatus httpStatus){
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getHttpStatus(){
+        return httpStatus;
+    }
+}
