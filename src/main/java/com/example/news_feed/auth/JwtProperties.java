b52011add12878f1a4d8eac5c6c006e702b6dc91
tgg <qazplmtfgcv123@gmail.com>
2025-02-15 12:01:03 +0900

    Chore: JwtProperties 수정 JwtProvider 수정

--- src/main/java/com/example/news_feed/auth/JwtProperties.java
+++ src/main/java/com/example/news_feed/auth/JwtProperties.java
@@ -1,10 +1,12 @@
 package com.example.news_feed.auth;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
 @PropertySource("classpath:application-jwt/properties")
 public class JwtProperties {
 
--- src/main/java/com/example/news_feed/auth/JwtProvider.java
+++ src/main/java/com/example/news_feed/auth/JwtProvider.java
@@ -23,35 +23,26 @@ public class JwtProvider {
     public static final long REFRESHTOKEN_TIME = 1000 * 60 * 60 * 24 * 14; // 2주
 
     public static final String ACCESS_PREFIX_STRING = "Bearer ";
-    public static final String ACCESS_HEADER_STRING = "Authorization";
-    public static final String REFRESH_HEADER_STRING = "RefreshToken";
 
     private final AccessTokenRepository accessTokenRepository;
     private final RefreshTokenRepository refreshTokenRepository;
     private final JwtProperties jwtProperties;
 
-    public String createAccessToken(Long memberId, String name, Boolean isAdmin){
-
-        jwtProperties.get
-
-        Map<String, Object> claims = new HashMap<>();
-
-        claims.put("memberId", memberId);
-        claims.put("nickname", name);
-        claims.put("isAdmin", Boolean.toString(isAdmin));
+    public String createAccessToken(Long memberId){
 
         Date expiration = new Date(System.currentTimeMillis() + ACCESSTOKEN_TIME);
 
         String accessToken = ACCESS_PREFIX_STRING + Jwts.builder()
                 .setSubject(Long.toString(memberId))
-                .addClaims(claims)
                 .setExpiration(expiration)
-                .signWith(this.getSigningKey())
+                .signWith(getSigningKey())
                 .compact();
 
-//        accessTokenRepository.deleteAccessTokenByMemberId(memberId);
-//        accessTokenRepository.addAccessToken(new AccessTokenDto(memberId, accessToken, expiration));
-
         return accessToken;
+    }
+
+    private SecretKey getSigningKey() {
+        String secret = jwtProperties.getSecretKey(); // 32바이트 이상 필수
+        return Keys.hmacShaKeyFor(secret.getBytes());
     }
 }
