df23ba225329b2c463dfe8df82565d4906abbfd7
tgg <qazplmtfgcv123@gmail.com>
2025-02-15 23:20:57 +0900

    Chore: 토큰 형식 검사하는 로직 작성

--- src/main/java/com/example/news_feed/common/interceptor/AuthInterceptor.java
+++ src/main/java/com/example/news_feed/common/interceptor/AuthInterceptor.java
@@ -2,6 +2,7 @@ package com.example.news_feed.common.interceptor;
 
 import com.example.news_feed.auth.JwtUtil;
 import com.example.news_feed.common.error.ErrorCode;
+import com.example.news_feed.common.error.exception.Exception400;
 import com.example.news_feed.common.error.exception.Exception401;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,9 +26,27 @@ public class AuthInterceptor implements HandlerInterceptor {
         String[] strs = uri.split("/");
 
         if(token != null){
+
+            // 토큰 형식 올바른지 확인
+            if(!(token.startsWith("access ") || token.startsWith("refresh"))){
+                throw new Exception400(ErrorCode.JWT_FORMAT_ERROR);
+            }
+
             if(strs[strs.length-1].equals("logout")){
+                if(token.startsWith("access ")){
+                    token = token.replace("access ", "");
+                }
+                else if(token.startsWith("refresh ")){
+                    token = token.replace("refresh ", "");
+                }
                 Claims claims = jwtUtil.getClaims(token);
                 request.setAttribute("memberId", claims.getSubject());
+            }
+            else if(strs[strs.length-1].equals("members") && method.equals("PATCH")){
+
+            }
+            else if(strs[strs.length-1].equals("members") && method.equals("DELETE")){
+
             }
         }
         else{
--- src/main/java/com/example/news_feed/member/controller/MemberController.java
+++ src/main/java/com/example/news_feed/member/controller/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/members")
+@RequestMapping("/api/members")
 public class MemberController {
 
     private final MemberService memberService;
