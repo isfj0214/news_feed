f582284eb084839d423dc4d75fbdafa9fc39a690
suyeonkim <suyeonk56@gmail.com>
2025-02-17 14:28:06 +0900

    Feat: comment crud 추가

---
+++ src/main/java/com/example/news_feed/comment/entity/Comment.java
@@ -0,0 +1,38 @@
+package com.example.news_feed.comment.entity;
+
+import com.example.news_feed.common.base.BaseEntity;
+import com.example.news_feed.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "comments")
+public class Comment extends BaseEntity {
+
+    @Id
+    private Long id;
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    //게시물 외래키 추가
+
+
+    public Comment(String contents, Member member) {
+
+    }
+
+    public Comment()
+    {
+
+    }
+
+    public void setContents(String contents)
+    {
+        this.contents = contents;
+    }
+
+}
