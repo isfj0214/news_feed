e400380d5ca61fdd6456955723f86f6e1d9257f5
hyos <hyozzang99@gmail.com>
2025-02-19 15:07:45 +0900

    Feat: de 게시물 삭제 작성, 게시물 ì¡ì 게시물 페ì이징 기능 작성

--- src/main/java/com/example/news_feed/post/repository/PostRepository.java
+++ src/main/java/com/example/news_feed/post/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package com.example.news_feed.post.repository;
 
 import com.example.news_feed.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+//    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
+    Page<Post> findAll(Pageable pageable);
 }
