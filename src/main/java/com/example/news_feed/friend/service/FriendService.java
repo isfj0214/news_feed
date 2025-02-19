e5c02342115394bf62417ceeb1cd2da6e612f160
kyn1013 <yn112511@naver.com>
2025-02-19 11:45:25 +0900

    Refactor : 검증 로직 메서드로 분리

--- src/main/java/com/example/news_feed/friend/service/FriendService.java
+++ src/main/java/com/example/news_feed/friend/service/FriendService.java
@@ -22,43 +22,18 @@ public class FriendService {
     private final FriendRepository friendRepository;
     private final MemberRepository memberRepository;
 
-
     @Transactional
     public FriendshipResponseDto request(FriendshipRequestDto friendshipRequestDto) {
-        //TODO : 친구 재신청 방지 기능 추가
-        /*
-         * 4. 이미 친구인데 친구 요청을 보낼 때, 안된다고 하는 경우
-         */
-        Long count = friendRepository.isFriend(friendshipRequestDto.getFromId(), friendshipRequestDto.getToId());
-        if (count == 1){
-            throw new RuntimeException("이미 친구로 등록된 친구입니다.");
-        }
+        isAlreadyFriend(friendshipRequestDto);
+        isSelfFriendRequest(friendshipRequestDto);
+        isFriendRequestExists(friendshipRequestDto);
 
-        /*
-         * 3. 자기 자신한테 친구 요청을 보낼 때, 안된다고 하는 경우
-         */
-        if (friendshipRequestDto.getFromId().equals(friendshipRequestDto.getToId())){
-            throw new RuntimeException("자기 자신에게는 친구 요청을 보낼 수 없습니다.");
-        }
-
-        /*
-         * 1. 1이 2한테 친구 요청을 보냈는데, 2가 1한테 친구요청을 보내려고 할 때 안된다고 하는 경우
-         * 2. 1이 2한테 친구 요청을 보냈는데, 한번 더 동일한 친구 요청을 보내는 경우에 안된다고 하는 경우
-         */
-        if(friendRepository.existsByFromIdAndMemberId(friendshipRequestDto.getFromId(), friendshipRequestDto.getToId())){
-            throw new RuntimeException("이미 존재하는 친구 요청이 있습니다.");
-        }
-
-
-        /*
-         * 친구 요청 생성
-         */
-        // 1. A가 B한테 친구신청을 보내서, from_id : a, to_id : b, is_friend : True를 insert
+        // 1. 정방향 데이터 추가 : A가 B한테 친구신청을 보내서, from_id : a, to_id : b, is_friend : True를 insert
         Member member = memberRepository.findById(friendshipRequestDto.getToId()).orElseThrow(() -> new RuntimeException("회원 없음"));
         Friend friend = new Friend(friendshipRequestDto.getFromId(), member, true);
         Friend savedFriend = friendRepository.save(friend);
 
-        // 2. 역방향 데이터 추가 from_id : b, to_id : a, is_friend : False를 insert
+        // 2. 역방향 데이터 추가 : from_id : b, to_id : a, is_friend : False를 insert
         Member reversedMember = memberRepository.findById(friendshipRequestDto.getFromId()).orElseThrow(() -> new RuntimeException("회원 없음"));
         Friend reversedFriend = new Friend(friendshipRequestDto.getToId(), reversedMember, false);
         friendRepository.save(reversedFriend);
@@ -73,35 +48,21 @@ public class FriendService {
     public void cancel(FriendshipCancelDto friendshipCancelDto) {
         memberRepository.findById(friendshipCancelDto.getFromId()).orElseThrow(() -> new RuntimeException("회원 없음"));
         memberRepository.findById(friendshipCancelDto.getToId()).orElseThrow(() -> new RuntimeException("회원 없음"));
-        // 존재하지 않는 친구 신청(내가 보낸 요청)을 취소하려고 하는 경우
-        List<Long> friendRequestList = friendRepository.getFriendRequestList(friendshipCancelDto.getFromId());
-        if (!friendRequestList.contains(friendshipCancelDto.getToId())){
-            throw new RuntimeException("존재하지 않는 친구 신청입니다.");
-        }
+        isFriendRequestExists(friendshipCancelDto);
         friendRepository.deleteFriendship(friendshipCancelDto.getToId(), friendshipCancelDto.getFromId());
     }
 
-
     @Transactional
     public void accept(FriendAcceptRequestDto acceptRequestDto) {
-        // 존재하지 않는 친구 신청(내가 받은 요청)을 취소하려고 하는 경우
-        List<Long> friendReceivedtList = friendRepository.getFriendReceivedList(acceptRequestDto.getFromId());
-        if (!friendReceivedtList.contains(acceptRequestDto.getToId())){
-            throw new RuntimeException("존재하지 않는 친구 신청입니다.");
-        }
+        isFriendReceivedExists(acceptRequestDto);
         friendRepository.acceptFriendRequest(acceptRequestDto.getFromId(), acceptRequestDto.getToId());
     }
 
     @Transactional
     public void delete(FriendshipDeleteDto friendshipDeleteDto) {
-        // 존재하지 않는 친구 신청(내가 받은 요청)을 취소하려고 하는 경우
-        List<Long> friendList = friendRepository.getFriendList(friendshipDeleteDto.getFromId());
-        if (!friendList.contains(friendshipDeleteDto.getToId())){
-            throw new RuntimeException("존재하지 않는 친구는 삭제할 수 없습니다.");
-        }
+        isFriends(friendshipDeleteDto);
         friendRepository.deleteFriendship(friendshipDeleteDto.getToId(), friendshipDeleteDto.getFromId());
     }
-
 
     @Transactional(readOnly = true)
     public List<Long> getFriendRequestList(Long id) {
@@ -121,4 +82,64 @@ public class FriendService {
         return friendList;
     }
 
+    /*
+     * 검증 메서드 - 찬구 신청시 검증
+     */
+    private void isFriendRequestExists(FriendshipRequestDto friendshipRequestDto) {
+        /*
+         * 1. A가 B한테 친구 요청을 보냈는데, B가 A한테 친구요청을 보내려고 할 때 안된다고 하는 경우
+         * 2. A가 B한테 친구 요청을 보냈는데, 한번 더 동일한 친구 요청(A가 B한테 친구 요청)을 보내는 경우에 안된다고 하는 경우
+         */
+        if(friendRepository.existsByFromIdAndMemberId(friendshipRequestDto.getFromId(), friendshipRequestDto.getToId())){
+            throw new RuntimeException("이미 존재하는 친구 요청이 있습니다.");
+        }
+    }
+
+    private static void isSelfFriendRequest(FriendshipRequestDto friendshipRequestDto) {
+        // 3. 자기 자신한테 친구 요청을 보낼 때(A가 A에게 친구 요청), 안된다고 하는 경우
+        if (friendshipRequestDto.getFromId().equals(friendshipRequestDto.getToId())){
+            throw new RuntimeException("자기 자신에게는 친구 요청을 보낼 수 없습니다.");
+        }
+    }
+
+    private void isAlreadyFriend(FriendshipRequestDto friendshipRequestDto) {
+        //4. 이미 친구인데 친구 요청을 보낼 때, 안된다고 하는 경우
+        Long count = friendRepository.isFriend(friendshipRequestDto.getFromId(), friendshipRequestDto.getToId());
+        if (count == 1){
+            throw new RuntimeException("이미 친구로 등록된 친구입니다.");
+        }
+    }
+
+    /*
+     * 검증 메서드 - 찬구 요청 취소시 검증
+     */
+    private void isFriendRequestExists(FriendshipCancelDto friendshipCancelDto) {
+        // 존재하지 않는 친구 신청(내가 보낸 요청)을 취소하려고 하는 경우
+        List<Long> friendRequestList = friendRepository.getFriendRequestList(friendshipCancelDto.getFromId());
+        if (!friendRequestList.contains(friendshipCancelDto.getToId())){
+            throw new RuntimeException("존재하지 않는 친구 신청입니다.");
+        }
+    }
+
+    /*
+     * 검증 메서드 - 찬구 요청 수락시 검증
+     */
+    private void isFriendReceivedExists(FriendAcceptRequestDto acceptRequestDto) {
+        // 존재하지 않는 친구 신청(내가 받은 요청)을 수락하려고 하는 경우
+        List<Long> friendReceivedtList = friendRepository.getFriendReceivedList(acceptRequestDto.getFromId());
+        if (!friendReceivedtList.contains(acceptRequestDto.getToId())){
+            throw new RuntimeException("존재하지 않는 친구 신청입니다.");
+        }
+    }
+
+    /*
+     * 검증 메서드 - 찬구 삭제시 검증
+     */
+    private void isFriends(FriendshipDeleteDto friendshipDeleteDto) {
+        // 친구가 아닌 사람을 삭제하려고 하는 경우
+        List<Long> friendList = friendRepository.getFriendList(friendshipDeleteDto.getFromId());
+        if (!friendList.contains(friendshipDeleteDto.getToId())){
+            throw new RuntimeException("존재하지 않는 친구는 삭제할 수 없습니다.");
+        }
+    }
 }
