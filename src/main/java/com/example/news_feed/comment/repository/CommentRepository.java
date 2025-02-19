de7b03f2885577b07737c9a6093fc2fd7093083a
kyn1013 <yn112511@naver.com>
2025-02-19 23:52:20 +0900

    Fix : 게시물 삭제시 관련 엔티티도 삭제되도록 수정

--- src/main/java/com/example/news_feed/comment/repository/CommentRepository.java
+++ src/main/java/com/example/news_feed/comment/repository/CommentRepository.java
@@ -2,11 +2,23 @@ package com.example.news_feed.comment.repository;
 
 import com.example.news_feed.comment.entity.Comment;
 import com.example.news_feed.member.entity.Member;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT c.id FROM Comment c WHERE c.post.id = :postId")
+    List<Long> findIdsByPostId(@Param("postId") Long postId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Comment c WHERE c.post.id = :postId")
+    void deleteByPostId(@Param("postId") Long postId);
 }
--- src/main/java/com/example/news_feed/comment/repository/LikeCommentRepository.java
+++ src/main/java/com/example/news_feed/comment/repository/LikeCommentRepository.java
@@ -4,8 +4,19 @@ import com.example.news_feed.comment.entity.Comment;
 import com.example.news_feed.comment.entity.LikeComment;
 import com.example.news_feed.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface LikeCommentRepository extends JpaRepository<LikeComment, Long> {
     // 특정 멤버가 특정 댓글에 좋아요를 눌렀는지 확인
     boolean existsByMemberAndComment(Member member, Comment comment);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM LikeComment lc WHERE lc.comment.id IN :commentIds")
+    void deleteByCommentIds(@Param("commentIds") List<Long> commentIds);
 }
